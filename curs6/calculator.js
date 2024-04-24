fdc9809a827aaba0ea2e007ee4d8406d191ce064
aadiaconitei <aadiaconitei@yahoo.com>
2024-04-24 19:15:59 +0300

    curs6

---
+++ curs6/calculator.js
@@ -0,0 +1,20 @@
+function sum(a, b) {
+  return a + b;
+}
+function diff(a, b) {
+  return a - b;
+}
+function product(a, b) {
+  return a * b;
+}
+module.exports = {
+  sum,
+  diff,
+  product,
+};
+
+// sau
+// export default { sum, diff, product}
+
+// node calculator.js
+// console.log(process);
